78e1203aa1f5470a20c9822435465d8e1fbd2332
Tiredestest <sho8029@gmail.com>
2024-06-22 20:59:04 +0900

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>🌟</Emoji> Special Thanks To
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
